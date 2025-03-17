06b9964614600c8df60e200776320a41ceab61ca
tulio <tulio89lopes@gmail.com>
2025-03-16 21:28:58 -0300

    Atualizando

--- main.c
+++ main.c
@@ -11,9 +11,9 @@ typedef struct {
 } TEvento;
 
 typedef struct {
-    int id;
     char* nome_cidade;
-    TEvento evento;
+    TEvento evento[5];
+    int qtd_eventos;
 } TCidade;
 
 typedef struct cel{
@@ -29,13 +29,6 @@ typedef struct arvore{
 
 void Cidade_aleatoria(TCel** x, char* cidade[], int num_cidades);
 void Inserir(TCel **x, TCel *pai, TCidade item);
-
-TCidade Ler_cidade(char* cidade, int id) {
-    TCidade c;
-    c.nome_cidade = cidade;
-    c.id = id;
-    return c;
-}
 
 void Cidade_aleatoria(TCel** x, char* cidade[], int num_cidades) {
     char* copia_cidade[num_cidades];
@@ -63,6 +56,23 @@ void Cidade_aleatoria(TCel** x, char* cidade[], int num_cidades) {
     }
 }
 
+void Evento_aleatorio(TCel* x, char* eventos[], int num_eventos) {
+    if (x == NULL)
+        return;
+
+    int num = (rand() % 5) + 1;
+    x->item.qtd_eventos = num;
+
+    for (int i = 0; i < num; i++) {
+        int indice = rand() % num_eventos;
+        x->item.evento[i].nome_evento = eventos[indice];
+        x->item.evento[i].ev_avaliacao = ((float)rand() / RAND_MAX) * 10.0;
+    }
+
+    Evento_aleatorio(x->esq, eventos, num_eventos);
+    Evento_aleatorio(x->dir, eventos, num_eventos);
+}
+
 TCel* CriaNo(TCidade item) {
     TCel *novoNo = (TCel*)malloc(sizeof(TCel));
     novoNo->item = item;
@@ -81,7 +91,7 @@ void Inserir(TCel **x, TCel *pai, TCidade item) {
     TCel *aux = *x;
     while (aux != NULL) {
         int cmp = strcmp(item.nome_cidade, aux->item.nome_cidade);
-        if (cmp < 0 || (cmp == 0 && item.id < aux->item.id)) {
+        if (cmp < 0 || (cmp == 0)) {
             if (aux->esq == NULL) {
                 aux->esq = CriaNo(item);
                 aux->esq->pai = aux;
@@ -159,178 +169,300 @@ void Retirar (TArvore *arvore, TCel **z){
     *z = NULL;
 }
 
-/*
-void Bubblesort(TCidade C[], int n){
- TCidade x;
- int i,j;
- for(i = 1; i < n; i++){
-  for(j = n; j > i; j--){
-     if(C[j].id < C[j - 1].id){
-        x = C[j];
-        C[j] = C[j - 1];
-        C[j - 1] = x;
-     }
-  }
- }
-}
-
-void SelectionSort(TCidade*C, int n){
-    int i,j,Min;
-    TCidade x;
-    for(i = 1; i <= n - 1; i++){
-        Min = i;
-        for(j = i + 1; j <= n; j++)
-            if(C[j].id < C[Min].id){
-                Min = j;
-        }
-        x = C[Min];
-        C[Min] = C[i];
-        C[i] = x;
+void BubbleSort(TCel* x) {
+    if (x == NULL) {
+        return;
     }
+
+    TEvento eventos[5];
+    int qtd_eventos = x->item.qtd_eventos;
+    for (int i = 0; i < qtd_eventos; i++) {
+        eventos[i] = x->item.evento[i];
+    }
+
+    for (int k = qtd_eventos - 1; k > 0; k--) {
+        for (int j = 0; j < k; j++) {
+            if (eventos[j].ev_avaliacao < eventos[j + 1].ev_avaliacao) {
+                TEvento aux = eventos[j];
+                eventos[j] = eventos[j + 1];
+                eventos[j + 1] = aux;
+            }
+        }
+    }
+
+    printf("\n%s:\n", x->item.nome_cidade);
+    for (int i = 0; i < qtd_eventos; i++) {
+        printf("\t%s - %.2f\n", eventos[i].nome_evento, eventos[i].ev_avaliacao);
+    }
+    printf("\n");
+    BubbleSort(x->esq);
+    BubbleSort(x->dir);
 }
 
-void InsertionSort(TCidade *C, int n){
-    int i,j;
-    TCidade x;
-    for(i = 2; i <= n; i++){
-        x = C[i];
-        j = i - 1;
-        j = i - 1;
-        C[0] = x;
-        while(x.id < C[j].id){
-            C[j + 1] = C[j];
+void SelectionSort(TCel* x){
+    if (x == NULL) {
+        return;
+    }
+
+    TEvento eventos[5];
+    int qtd_eventos = x->item.qtd_eventos;
+    for (int i = 0; i < qtd_eventos; i++) {
+        eventos[i] = x->item.evento[i];
+    }
+
+    for(int i = 0; i < qtd_eventos - 1; i++){
+        int Max = i;
+        for(int j = i + 1; j < qtd_eventos; j++)
+            if(eventos[j].ev_avaliacao > eventos[Max].ev_avaliacao){
+                Max = j;
+        }
+        if (eventos[i].ev_avaliacao != eventos[Max].ev_avaliacao) {
+            TEvento aux = eventos[Max];
+            eventos[Max] = eventos[i];
+            eventos[i] = aux;
+        }
+    }
+
+    printf("\n%s:\n", x->item.nome_cidade);
+    for (int i = 0; i < qtd_eventos; i++) {
+        printf("\t%s - %.2f\n", eventos[i].nome_evento, eventos[i].ev_avaliacao);
+    }
+    printf("\n");
+    SelectionSort(x->esq);
+    SelectionSort(x->dir);
+}
+
+void InsertionSort(TCel* x){
+    if (x == NULL) {
+        return;
+    }
+
+    TEvento eventos[5];
+    int qtd_eventos = x->item.qtd_eventos;
+    for (int i = 0; i < qtd_eventos; i++) {
+        eventos[i] = x->item.evento[i];
+    }
+
+    for(int i = 1; i < qtd_eventos; i++){
+        TEvento aux = eventos[i];
+        int j = i - 1;
+        while((j >= 0) && (eventos[j].ev_avaliacao < aux.ev_avaliacao)){
+            eventos[j + 1] = eventos[j];
             j--;
         }
-        C[j + 1] = x;
+        eventos[j + 1] = aux;
+        j++;
     }
+
+    printf("\n%s:\n", x->item.nome_cidade);
+    for (int i = 0; i < qtd_eventos; i++) {
+        printf("\t%s - %.2f\n", eventos[i].nome_evento, eventos[i].ev_avaliacao);
+    }
+    printf("\n");
+    InsertionSort(x->esq);
+    InsertionSort(x->dir);
 }
 
-void ShellSort(TCidade *C, int n){
-    int i, j; int h = 1;
-    TCidade x;
+void ShellSort(TCel* x){
+    if (x == NULL) {
+        return;
+    }
+
+    TEvento eventos[5];
+    int qtd_eventos = x->item.qtd_eventos;
+    for (int i = 0; i < qtd_eventos; i++) {
+        eventos[i] = x->item.evento[i];
+    }
+
+    int h = 1;
     do{
         h = h * 3 + 1;
-    }while(h < n);
+    } while(h < qtd_eventos);
     do{
         h /= 3;
-        for(i = h + 1; i <= n; i++){
-            x = C[i];
-            j = i;
-            while(C[j - h].id > x.id){
-                C[j] = C[j - h];
+        for(int i = h; i < qtd_eventos; i++){
+            TEvento aux = eventos[i];
+            int j = i;
+            while(eventos[j - h].ev_avaliacao < aux.ev_avaliacao){
+                eventos[j] = eventos[j - h];
                 j -= h;
-                if(j <= h)
-                    break;
             }
-            C[j] = x;
+            eventos[j] = aux;
         }
     } while(h != 1);
+
+    printf("\n%s:\n", x->item.nome_cidade);
+    for (int i = 0; i < qtd_eventos; i++) {
+        printf("\t%s - %.2f\n", eventos[i].nome_evento, eventos[i].ev_avaliacao);
+    }
+
+    ShellSort(x->esq);
+    ShellSort(x->dir);
 }
 
-int Particao(TCidade *C, int p, int r) {
-    TCidade x = C[r];
+int Particao(TCel*x , int p, int r) {
+    if (x == NULL) {
+        return -1;
+    }
+    TEvento* eventos = x->item.evento;
+    TEvento aux1 = eventos[r];
     int i = p - 1;
+
     for (int j = p; j < r; j++) {
-        if (C[j].id <= x.id) {
+        if (eventos[j].ev_avaliacao >= aux1.ev_avaliacao) {
             i++;
-            TCidade aux = C[i];
-            C[i] = C[j];
-            C[j] = aux;
+
+            TEvento aux2 = eventos[i];
+            eventos[i] = eventos[j];
+            eventos[j] = aux2;
         }
     }
-    TCidade aux = C[i + 1];
-    C[i + 1] = C[r];
-    C[r] = aux;
+
+    TEvento aux2 = eventos[i + 1];
+    eventos[i + 1] = eventos[r];
+    eventos[r] = aux2;
+
     return i + 1;
 }
 
-void QuickSort(TCidade *C, int p, int r){
-    if( p < r){
-        int q = Particao(C, p, r);
-        QuickSort(C, p, q - 1);
-        QuickSort(C, q + 1, r);
+void QuickSort(TCel* x, int p, int r) {
+    if (x == NULL || p >= r) {
+        return;
     }
+    int q = Particao(x, p, r);
+    QuickSort(x, p, q - 1);
+    QuickSort(x, q + 1, r);
 }
 
-void Merge(TCidade *C, int l, int m, int r){
+void Imprime_QuickSort(TCel* x) {
+    if (x == NULL) {
+        return;
+    }
+
+    QuickSort(x, 0, x->item.qtd_eventos - 1);
+
+    printf("\n%s:\n", x->item.nome_cidade);
+    for (int i = 0; i < x->item.qtd_eventos; i++) {
+        printf("\t%s - %.2f\n", x->item.evento[i].nome_evento, x->item.evento[i].ev_avaliacao);
+    }
+    printf("\n");
+    Imprime_QuickSort(x->esq);
+    Imprime_QuickSort(x->dir);
+}
+
+void Merge(TCel* x, int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
-    TCidade L[n1], R[n2];  //
 
-    for (int i = 0; i < n1; i++)
-        L[i] = C[l + i];
-    for (int j = 0; j < n2; j++)
-        R[j] = C[m + 1 + j];
+    TEvento L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++) {
+        L[i] = x->item.evento[l + i];
+    }
+    for (int j = 0; j < n2; j++) {
+        R[j] = x->item.evento[m + 1 + j];
+    }
 
     int i = 0, j = 0, k = l;
     while (i < n1 && j < n2) {
-        if (L[i].id <= R[j].id) {
-            C[k] = L[i];
+        if (L[i].ev_avaliacao >= R[j].ev_avaliacao) {
+            x->item.evento[k] = L[i];
             i++;
         } else {
-            C[k] = R[j];
+            x->item.evento[k] = R[j];
             j++;
         }
         k++;
     }
 
     while (i < n1) {
-        C[k] = L[i];
+        x->item.evento[k] = L[i];
         i++;
         k++;
     }
 
     while (j < n2) {
-        C[k] = R[j];
+        x->item.evento[k] = R[j];
         j++;
         k++;
     }
 }
 
-void Mergesort(TCidade *C, int l, int r) {
-    if (l >= r)
+void MergeSort(TCel* x, int l, int r) {
+    if (l >= r) {
         return;
+    }
     int m = l + (r - l) / 2;
-    Mergesort(C, l, m);
-    Mergesort(C, m + 1, r);
-    Merge(C, l, m, r);
+    MergeSort(x, l, m);
+    MergeSort(x, m + 1, r);
+    Merge(x, l, m, r);
 }
 
-void MaxHeapify(TCidade *C, int i, int n){
-    TCidade aux; int esq = 2*i; int dir = 2*i+1; int maior;
-    if((C[i].id < C[esq].id) && (esq <= n) && esq >= 1)
-        maior = esq;
-    else
-        maior = i;
-    if((C[maior].id < C[dir].id) && (dir <= n) && dir >= 1)
-        maior = dir;
-    if(maior != i){
-        aux = C[maior];
-        C[maior] = C[i];
-        MaxHeapify(C, maior, n);
+void Imprimir_MergeSort(TCel* x) {
+    if (x == NULL) {
+        return;
+    }
+    MergeSort(x, 0, x->item.qtd_eventos - 1);
+
+    printf("\n%s:\n", x->item.nome_cidade);
+    for (int i = 0; i < x->item.qtd_eventos; i++) {
+        printf("\t%s - %.2f\n", x->item.evento[i].nome_evento, x->item.evento[i].ev_avaliacao);
+    }
+    printf("\n");
+    Imprimir_MergeSort(x->esq);
+    Imprimir_MergeSort(x->dir);
+}
+
+void MaxHeapify(TCel* x, int i, int n) {
+    int esq = 2 * i + 1, dir = 2 * i + 2, menor = i;
+
+    if (esq < n && x->item.evento[esq].ev_avaliacao < x->item.evento[menor].ev_avaliacao) {
+        menor = esq;
+    }
+    if (dir < n && x->item.evento[dir].ev_avaliacao < x->item.evento[menor].ev_avaliacao) {
+        menor = dir;
+    }
+    if (menor != i) {
+        TEvento aux = x->item.evento[i];
+        x->item.evento[i] = x->item.evento[menor];
+        x->item.evento[menor] = aux;
+
+        MaxHeapify(x, menor, n);
     }
 }
 
-void BuildMaxHeap(TCidade *C, int n){
-    int i;
-    for(i = n/2; i > 0; i--){
-        MaxHeapify(C, i, n);
+void BuildMaxHeap(TCel* x, int n) {
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        MaxHeapify(x, i, n);
     }
 }
 
-void HeapSort(TCidade *C, int n) {
-    TCidade aux;
-    int tam = n;
-    BuildMaxHeap(C, n);
-    for (int i = n; i >= 2; i--) {
-        aux = C[1];
-        C[1] = C[i];
-        C[i] = aux;
-        tam--;
-        MaxHeapify(C, 1, tam);
+void HeapSort(TCel* x, int n) {
+    BuildMaxHeap(x, n);
+
+    for (int i = n - 1; i >= 0; i--) {
+        TEvento aux = x->item.evento[0];
+        x->item.evento[0] = x->item.evento[i];
+        x->item.evento[i] = aux;
+
+        MaxHeapify(x, 0, i);
     }
 }
-*/
+
+void Imprimir_HeapSort(TCel* x) {
+    if (x == NULL) {
+        return;
+    }
+    HeapSort(x, x->item.qtd_eventos);
+
+    printf("\n%s:\n", x->item.nome_cidade);
+    for (int i = 0; i < x->item.qtd_eventos; i++) {
+        printf("\t%s - %.2f\n", x->item.evento[i].nome_evento, x->item.evento[i].ev_avaliacao);
+    }
+    printf("\n");
+    Imprimir_HeapSort(x->esq);
+    Imprimir_HeapSort(x->dir);
+}
 
 // Impressão das cidades
 void Imprimir_cidades(TCel *x){
@@ -338,6 +470,21 @@ void Imprimir_cidades(TCel *x){
         Imprimir_cidades(x->esq);
         printf("%s, ", x->item.nome_cidade);
         Imprimir_cidades(x->dir);
+    }
+}
+
+void Imprimir_eventos(TCel *x){
+    if(x != NULL){
+        Imprimir_eventos(x->esq);
+        printf("\n%s: \n", x->item.nome_cidade);
+        for (int i = 0; i < x->item.qtd_eventos; i++) {
+            if (x->item.evento[i].nome_evento != NULL) {
+                printf("\n\t%s - %.2f", x->item.evento[i].nome_evento,
+                       x->item.evento[i].ev_avaliacao);
+            }
+        }
+        printf("\n");
+        Imprimir_eventos(x->dir);
     }
 }
 
@@ -367,6 +514,7 @@ void PosOrdem(TCel *x){
 
 int main() {
     setlocale(LC_ALL,"portuguese");
+srand(time(NULL));
 
     TArvore arvore;
     arvore.raiz = NULL;
@@ -375,10 +523,21 @@ int main() {
     int num_cidade = sizeof(cidade) / sizeof(cidade[0]);
     Cidade_aleatoria(&arvore.raiz, cidade, num_cidade);
 
+    char *eventos[] = {"Festival de Musica", "Festival de Artesanato", "Show ao Vivo", "Festival Gastronomico",
+    "Exposicao de Arte", "Exposicao de Museum", "Cinema ao ar Livre", "Festival de Comedia", "Festival da Cultura",
+    "Exposicao de Livros", "Festival do Foclore", "Festival do Vinho", "Festival do Fogo", "Festival da Danca",
+    "Exposicao Cientifica"};
+    int num_eventos = sizeof(eventos) / sizeof(eventos[0]);
+    Evento_aleatorio(arvore.raiz, eventos, num_eventos);
+
     printf("Cidades: ");
     Imprimir_cidades(arvore.raiz);
+    printf("\n\n");
+
+    printf("Eventos em: ");
+    Imprimir_eventos(arvore.raiz);
     printf("\n");
-    printf("\n");
+
     printf("Pre-Order: ");
     PreOrdem(arvore.raiz);
     printf("\n");
@@ -392,12 +551,11 @@ int main() {
 
     int escolha;
     TCel* pesquisa = NULL;
-    TCidade i, c;
     bool loop = true;
 
-    printf("ARVORE BINARIA");
     while(loop) {
-        printf("\n\t1. Pesquisa pelo NOME\n\t2. Sair");
+        printf("ARVORE BINARIA");
+        printf("\n\t1. Pesquisa pelo NOME\n\t2. Visualizar metodos de ordenacao\n\t3. Sair");
         printf("\nEscolha: ");
         scanf("%d", &escolha);
         getchar();
@@ -415,6 +573,49 @@ int main() {
                 }
                 break;
             case 2:
+                printf("Escolha o metodo de ordenacao:");
+                printf("\n\t1. Bublle Sort\n\t2. Selection Sort\n\t3. Insertion Sort\n\t4. Shellsort\n\t5. Quixksort\n\t6. Mergesort\n\t7. Heapsort\n\t8. Sair");
+                printf("\nEscolha: ");
+                scanf("%d", &escolha);
+                getchar();
+                switch(escolha){
+                    case 1:
+                        printf("\nMETODO BUBBLE SORT:");
+                        BubbleSort(arvore.raiz);
+                        break;
+                    case 2:
+                        printf("\nMETODO SELECTION SORT:");
+                        SelectionSort(arvore.raiz);
+                        break;
+                    case 3:
+                        printf("\nMETODO INSERTION SORT:");
+                        InsertionSort(arvore.raiz);
+                        break;
+                    case 4:
+                        printf("\nMETODO SHELL SORT:");
+                        ShellSort(arvore.raiz);
+                        break;
+                    case 5:
+                        printf("\nMETODO QUICK SORT:");
+                        Imprime_QuickSort(arvore.raiz);
+                        break;
+                    case 6:
+                        printf("\nMETODO MERGE SORT:");
+                        Imprimir_MergeSort(arvore.raiz);
+                        break;
+                    case 7:
+                        printf("\nMETODO HEAP SORT:");
+                        Imprimir_HeapSort(arvore.raiz);
+                        break;
+                    case 8:
+                        printf("Saindo...\n");
+                        return;
+                        break;
+                    default:
+                        printf("Erro de digitacao! Digite novamente!\n");
+                }
+                break;
+            case 3:
                 printf("Saindo...\n");
                 loop = false;
                 break;
@@ -422,8 +623,6 @@ int main() {
                 printf("Erro de digitacao! Digite novamente!\n");
         }
     }
-
-
 
     return 0;
 }
