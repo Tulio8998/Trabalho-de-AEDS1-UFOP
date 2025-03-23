c6fde897fedca7cc18bca528b08a1bf20c7bdc79
tulio <tulio89lopes@gmail.com>
2025-03-23 00:07:34 -0300

    Removendo caracteres especias e atualizando a saida dos dados

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,9 +1,11 @@
-printf("\nDigite o NOME do EVENTO: ");
-                                            fgets(nome, sizeof(nome), stdin);
-                                            nome[strcspn(nome, "\n")] = '\0';
-                                            TEvento* evento = Pesquisar_evento_nome(&pesquisa->item, nome);
-                                            if (evento != NULL) {
-                                                printf("Foi encontrado o EVENTO [%s] na CIDADE [%s]\n", evento->nome_evento, pesquisa->item.nome_cidade);
-                                            } else {
-                                                printf("O EVENTO [%s] não foi encontrado na CIDADE [%s]\n", nome, pesquisa->item.nome_cidade);
-                                            }
+void Imprimir_cidades(TCel *x, int* seta){
+    if(x != NULL){
+        Imprimir_cidades(x->esq, seta);
+        if (*seta == 1) {
+            printf(", ");
+        }
+        printf("[%s]", x->item.nome_cidade);
+        *seta = 1;
+        Imprimir_cidades(x->dir, seta);
+    }
+}
